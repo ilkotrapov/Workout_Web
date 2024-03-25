a715bff2404dd917ec99613bd6c0c2175ade5d40
Ivan <ivanveselinov264@gmail.com>
2024-03-25 22:30:41 +0200

    Migrations

---
+++ Products_Web/Products_Web/Migrations/20240325202348_IvanIsHere.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Products_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class IvanIsHere : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
