2c76428ab37d879e93c7cda745aba45c06eb4f96
Ivan <ivanveselinov264@gmail.com>
2024-03-29 21:31:31 +0200

    Update CreateTrainerViewModel.cs

--- Products_Web/Products_Web/Models/Trainer/CreateTrainerViewModel.cs
+++ Products_Web/Products_Web/Models/Trainer/CreateTrainerViewModel.cs
@@ -5,5 +5,12 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Gym { get; set; }
+
+        public CreateTrainerViewModel(string name, string email, string gym)
+        {
+            Name = name;
+            Email = email;
+            Gym = gym;
+        }
     }
 }
