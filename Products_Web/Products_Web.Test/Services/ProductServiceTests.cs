b18bc3469ec2965f8f23b83769b47ee49ee69c20
Ilko Trapov <trapovilko@gmail.com>
2024-03-29 12:55:28 +0200

    123

--- Products_Web/Products_Web.Test/Services/ProductServiceTests.cs
+++ Products_Web/Products_Web.Test/Services/ProductServiceTests.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -131,4 +131,4 @@ namespace Products_Web.Tests.Services
             return productRepositoryMock;
         }
     }
-}*/
+}
--- Products_Web/Products_Web/Models/Product/CreateProductViewModel.cs
+++ Products_Web/Products_Web/Models/Product/CreateProductViewModel.cs
@@ -5,5 +5,12 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public int Stock { get; set; }
+
+        public CreateProductViewModel(string name, double price, int stock)
+        {
+            Name = name;
+            Price = price;
+            Stock = stock;
+        }
     }
 }
